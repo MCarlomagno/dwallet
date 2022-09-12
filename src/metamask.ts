dea18ab8f1fccd1b375033428bf43af83ff81408
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-09-12 18:08:09 -0300

    added metamask support

--- src/metamask.ts
+++ src/metamask.ts
@@ -1,4 +1,9 @@
-import { ExternalProvider, Web3Provider } from '@ethersproject/providers';
+import {
+  ExternalProvider,
+  JsonRpcSigner,
+  Network,
+  Web3Provider } from '@ethersproject/providers';
+import { parseUnits } from '@ethersproject/units';
 
 declare global {
   interface Window {
@@ -6,12 +11,44 @@ declare global {
   }
 };
 
+export enum Methods {
+  SendTransaction = 'eth_sendTransaction'
+}
+
+export class Connection {
+  network: Network;
+  accounts: string[];
+  signer: JsonRpcSigner;
+  constructor(accounts: string[], network: Network, signer: JsonRpcSigner) {
+    this.accounts = accounts;
+    this.network = network;
+    this.signer = signer;
+  }
+}
+
 export class Metamask {
-  readonly provider: Web3Provider;
+  provider: Web3Provider;
+  connection: Connection | undefined;
 
   constructor() {
     if (!window.ethereum) throw Error('Provider does not exist');
-  
     this.provider = new Web3Provider(window.ethereum);
+  }
+
+  async connect(): Promise<Connection> {
+    const accounts: string[] = await this.provider.send("eth_requestAccounts", []);
+    const network: Network = await this.provider.getNetwork();
+    const signer: JsonRpcSigner = this.provider.getSigner();
+    this.connection = new Connection(accounts, network, signer);
+    return this.connection;
+  }
+
+  async sendTransaction(from: string, to: string, valueInEther: string) {
+    const value = parseUnits(valueInEther, 'ether').toHexString();
+    const hash = await this.provider.send(
+      Methods.SendTransaction,
+      [{ from, to, value }]
+    );
+    return hash;
   }
 }
